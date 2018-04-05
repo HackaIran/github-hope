20ab118b3569ff65901beb021707966bd031bb00
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-06 01:44:51 +0430

    little change in argument name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /**
  * Represents main hope function
- * @param {string} repoURL url which will be checked
+ * @param {string} url a git repository's url which will be checked
  * @returns {Promise} a promise including test results
  */
-export default function hope (repoURL) {
+export default function hope (url) {
 
     return new Promise ((resolve, reject) => {
 
     })
-    
+
 }
