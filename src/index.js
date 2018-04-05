4b58cba390d8fd30c915ad26678d5d0001225139
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-06 01:38:52 +0430

    main function is more functional now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export default function hope() {
+/**
+ * Represents main hope function
+ * @param {string} repoURL url which will be checked
+ * @returns {Promise} a promise including test results
+ */
+export default function hope (repoURL) {
 
+    return new Promise ((resolve, reject) => {
+
+    })
+    
 }
