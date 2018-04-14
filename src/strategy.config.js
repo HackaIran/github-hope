e27b09a8ec9cb42110e0ad7f47a0ac1c2f1ac3b0
pmzi <pouya.mozafar@gmail.com>
2018-04-14 13:44:50 +0430

    strategy config file sample added

---
+++ src/strategy.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    general : {
+        readme : {
+            exist:{
+                heading : "Readme file exists!",
+                message : "Some message",
+                type : "success"
+            },
+            notExist:{
+                heading : "Readme file doesn't exist!",
+                message : "Some message",
+                type : "error"
+            }
+        }
+    }
+}
